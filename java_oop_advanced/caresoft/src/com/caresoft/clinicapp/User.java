44a1dd160a5e24a8e1b33f6404d34bbe8be22514
Jared McCoon <jaredmccoon@protonmail.com>
2022-04-12 14:49:07 -0700

    caresoft

--- java_oop_advanced/caresoft/src/com/caresoft/clinicapp/User.java
+++ java_oop_advanced/caresoft/src/com/caresoft/clinicapp/User.java
@@ -4,8 +4,7 @@ public class User {
 	protected Integer id;
 	protected int pin;
 	
-	//	//// CONSTRUCTOR ////////////////////////////////////////////
-	
+
 	public User(Integer id) {
 		this.id = id;
 	}
